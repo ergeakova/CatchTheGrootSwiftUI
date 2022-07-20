d3ba735fa05492787075716e9b95abf6131471d1
ergeakova <ergeak@gmail.com>
2022-07-20 19:05:50 +0300

    Initial Commit

---
+++ CatchTheGroot/CatchTheGrootApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchTheGrootApp.swift
+//  CatchTheGroot
+//
+//  Created by Erge Gevher Akova on 20.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct CatchTheGrootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
